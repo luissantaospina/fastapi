d7ba6ab52eb01ec6bf030c66a1bad88b918aa7f9
Luis Santa <luis@lizit.app>
2023-07-25 16:41:15 -0500

    Update requirements

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from ..helpers import create_access_token
 from fastapi import APIRouter
